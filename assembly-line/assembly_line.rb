b5768e5d9663bcab9400f4608ac3af9d9dd94d9a
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-04-11 12:30:43 +0200

    Refactor production rate calculation using case statement for improved readability

--- assembly-line/assembly_line.rb
+++ assembly-line/assembly_line.rb
@@ -1,18 +1,17 @@
 class AssemblyLine
+  MIN_PRODUCE_CARS_PER_HOUR = 221
+  
   def initialize(speed)
     @speed = speed
   end
 
   def production_rate_per_hour
     success_rate = 
-      if @speed <= 4
-        1
-      elsif @speed <= 8
-        0.9
-      elsif @speed == 9
-        0.8
-      elsif @speed == 10
-        0.77
+      case @speed  
+        when 1..4 then 1
+        when 5..8 then 0.9
+        when 9 then 0.8
+        when 10 then 0.77
       end  
   
     @speed * MIN_PRODUCE_CARS_PER_HOUR * success_rate 
@@ -21,8 +20,4 @@ class AssemblyLine
   def working_items_per_minute
     (production_rate_per_hour / 60).to_i
   end
-
-  private
-
-  MIN_PRODUCE_CARS_PER_HOUR = 221
 end
