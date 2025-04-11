a011a4ce4974c4a1442dff6ce92a85a91002f8b8
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-04-11 17:18:40 +0200

    Define IDENTIFIER constant and implement get_identifier and get_terminal methods; update tests to include minitest pride.

--- port-palermo/port_palermo.rb
+++ port-palermo/port_palermo.rb
@@ -1,11 +1,12 @@
 module Port
-  # TODO: define the 'IDENTIFIER' constant
+  IDENTIFIER = :PALE
 
   def self.get_identifier(city)
-    raise 'Please implement the Port.get_identifier method'
+    city[0,4].upcase.to_sym
   end
 
   def self.get_terminal(ship_identifier)
-    raise 'Please implement the Port.get_terminal method'
+    return :B unless ship_identifier[0,3].match?(/(OIL|GAS)/)
+    :A
   end
 end
