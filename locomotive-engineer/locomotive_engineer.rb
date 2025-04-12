0ac72099e8a07639aa0536e2b031e70e03d58cb3
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-04-12 18:14:19 +0200

    Implement methods in LocomotiveEngineer class and add tests for functionality

--- locomotive-engineer/locomotive_engineer.rb
+++ locomotive-engineer/locomotive_engineer.rb
@@ -1,17 +1,20 @@
 class LocomotiveEngineer
-  def self.generate_list_of_wagons
-    raise 'Please implement the LocomotiveEngineer.generate_list_of_wagons method'
+  def self.generate_list_of_wagons(*arguments)
+    arguments
   end
 
   def self.fix_list_of_wagons(each_wagons_id, missing_wagons)
-    raise 'Please implement the LocomotiveEngineer.fix_list_of_wagons method'
+    first, second, *other_wagon_ids = each_wagons_id
+    id_one, *wagon_ids = other_wagon_ids
+    
+    [id_one, *missing_wagons, *wagon_ids, first, second]
   end
 
-  def self.add_missing_stops
-    raise 'Please implement the LocomotiveEngineer.add_missing_stops method'
+  def self.add_missing_stops(route, **city_stops)
+    {**route, stops: [*city_stops.values] }
   end
 
   def self.extend_route_information(route, more_route_information)
-    raise 'Please implement the LocomotiveEngineer.extend_route_information method'
+    { **route, **more_route_information }
   end
 end
--- locomotive-engineer/locomotive_engineer_test.rb
+++ locomotive-engineer/locomotive_engineer_test.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+require 'minitest/pride'
 require_relative 'locomotive_engineer'
 
 class LocomotiveEngineerTest < Minitest::Test
