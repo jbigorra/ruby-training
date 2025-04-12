4c25bbc1d7220baa0ab351a5078227b0d21830c1
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-04-12 19:10:05 +0200

    Implement ResistorColorDuo module and update tests to remove skips

--- resistor-color-duo/resistor_color_duo.rb
+++ resistor-color-duo/resistor_color_duo.rb
@@ -1,7 +1,20 @@
-=begin
-Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
-`resistor_color_duo_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/resistor-color-duo` directory.
-=end
+module ResistorColorDuo
+  COLOR_CODES = Hash[
+    'black',  0,
+    'brown',  1,
+    'red',    2,
+    'orange', 3,
+    'yellow', 4,
+    'green',  5,
+    'blue',   6,
+    'violet', 7,
+    'grey',   8,
+    'white',  9,
+  ]
+  
+  def self.value(colors)
+    color_1, color_2 = colors
+    
+    (COLOR_CODES[color_1].to_s << COLOR_CODES[color_2].to_s).to_i
+  end
+end
