deb3843e1417b61683e28ccc971f6d4cecb2fb1d
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-04-12 22:17:06 +0200

    Refactor DndCharacter class and tests for improved clarity and functionality

--- dnd-character/dnd_character.rb
+++ dnd-character/dnd_character.rb
@@ -1,17 +1,28 @@
-=begin
-Write your code for the 'D&D Character' exercise in this file. Make the tests in
-`dnd_character_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/dnd-character` directory.
-=end
-
 class DndCharacter
-  def self.modifier
-    # Your code here
+  attr_reader :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma
+  attr_reader :hitpoints
+
+  BASE_HITPOINTS = 10
+
+  def self.modifier(constitution)
+    ((constitution - 10) / 2).truncate(0)
   end
 
   def initialize
-    # Your code here
+    @prng = Random.new(Random.new_seed)
+    @constitution = roll_4_dices_and_sum_top_3_values
+    @strength = roll_4_dices_and_sum_top_3_values
+    @dexterity = roll_4_dices_and_sum_top_3_values
+    @wisdom = roll_4_dices_and_sum_top_3_values
+    @intelligence = roll_4_dices_and_sum_top_3_values
+    @charisma = roll_4_dices_and_sum_top_3_values
+    @hitpoints = BASE_HITPOINTS + DndCharacter.modifier(constitution)
+  end
+
+  private 
+
+  def roll_4_dices_and_sum_top_3_values
+    rolls = [@prng.rand(1..6), @prng.rand(1..6), @prng.rand(1..6), @prng.rand(1..6)]
+    rolls.max(3).sum
   end
 end
