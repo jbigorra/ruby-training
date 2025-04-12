1f52f3f2310b06aee75dec7cbaba905aa42b258a
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-04-12 10:47:48 +0200

    Implement BirdCount methods and add minitest pride for improved testing clarity

--- bird-count/bird_count.rb
+++ bird-count/bird_count.rb
@@ -1,25 +1,29 @@
 class BirdCount
   def self.last_week
-    raise 'Please implement the BirdCount.last_week method'
+    [0, 2, 5, 3, 7, 8, 4]
   end
 
   def initialize(birds_per_day)
-    raise 'Please implement the BirdCount#initialize method'
+    @birds_per_day = birds_per_day
   end
 
   def yesterday
-    raise 'Please implement the BirdCount#yesterday method'
+    @birds_per_day[-2]
   end
 
   def total
-    raise 'Please implement the BirdCount#total method'
+    @birds_per_day.sum()
   end
 
   def busy_days
-    raise 'Please implement the BirdCount#busy_days method'
+    busy_days = @birds_per_day.select do |count|
+      count >= 5
+    end
+
+    busy_days.size
   end
 
   def day_without_birds?
-    raise 'Please implement the BirdCount#day_without_birds method'
+    @birds_per_day.include?(0)
   end
 end
--- bird-count/bird_count_test.rb
+++ bird-count/bird_count_test.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+require 'minitest/pride'
 require_relative 'bird_count'
 
 class BirdCountTest < Minitest::Test
