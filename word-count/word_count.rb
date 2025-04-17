25793ceecb8039bc6ad2c64b8f0c4ce83b7fd498
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-04-17 19:56:48 +0200

    Implement Phrase class and initial word count functionality with tests

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,7 +1,15 @@
-=begin
-Write your code for the 'Word Count' exercise in this file. Make the tests in
-`word_count_test.rb` pass.
+class Phrase
+  attr_reader :phrase
 
-To get started with TDD, see the `README.md` file in your
-`ruby/word-count` directory.
-=end
+  def initialize(phrase)
+    @phrase = phrase.downcase.scan(/\b[\w']+\b/)
+  end
+
+  def word_count
+    phrase.each_with_object({}) do |value, obj|
+      obj[value] ||= 0
+      obj[value] += 1
+      obj
+    end
+  end
+end
